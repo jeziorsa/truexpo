57ec5c8dbe8c8c835d7d613685636e75e555604f
Adam Jeziorski <biuro@atsa.pl>
2012-05-04 02:19:00 +0200

    spac

--- app/views.py
+++ app/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from app.models import Project, Image
+from django.template.context import RequestContext
 
 def index(request):
     latest_project_list = Project.objects.all().order_by('-pub_date')[:5]
     return render_to_response('list.html', {'latest_project_list': latest_project_list})
 
 def detail(request, project_id):
-    images_list = Image.objects.all().order_by('-pub_date')[:5]
-    return render_to_response('list.html', {'images_list': images_list})
+    images_list = Image.objects.all()
+    return render_to_response('project.html', {'images_list': images_list})
 
