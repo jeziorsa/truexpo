4f403ef20550f8c8b933cf639beee1d6b611f882
Adam Jeziorski <biuro@atsa.pl>
2012-05-08 19:38:15 +0200

    nice view, sir!

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from app.models import Project, Image, ImageForm
 from django.template.context import RequestContext
@@ -16,10 +16,15 @@ def detail(request, project_id):
 
 def upload(request, project_id):
     if request.method == 'POST': # If the form has been submitted...
-        form = ImageForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
-            form.save()
-	    return HttpResponseRedirect('/') # Redirect after POST
+        form = ImageForm(request.POST, request.FILES) # A form bound to the POST data
+	print 'xxx'
+        #if form.is_valid(): # All validation rules pass
+        print form.errors
+	print request.POST
+	form.save()
+	print 'xxx2'
+	
+	return HttpResponseRedirect('/') # Redirect after POST
     else:
         form = ImageForm() # An unbound form
 
